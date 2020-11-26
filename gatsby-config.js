e8c6176603deb815c6600448052c27602568f5fc
techwithe <emmanuel.ogbewe12@gmail.com>
2020-11-25 22:10:57 -0500

    Add plugin to handle updating manfiest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,24 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Serge",
+        short_name: "Serge",
+        start_url: "/",
+        background_color: "#050505",
+        theme_color: "#050505",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/serge_icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
